e2020b2bb6361055fcbd1c4ef42275cd7bae406e
Nishtha Rajiv Sainger <63958583+Nishtha2709@users.noreply.github.com>
2021-10-28 02:09:21 +0530

    Delete FTProject.R

--- FTProject.R
+++
@@ -1,9 +0,0 @@
-library(quantmod)
-library(tseries)
-library(timeseries)
-library(forecast)
-library(xts)
-install.packages("timeSeries")
-
-getSymbols('SPY',from='24-01-01',to='2019-09-09')
-class(SPY)
